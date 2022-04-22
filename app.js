30ea08b3149ec468c9bda2fc7fa5e2006edb58b9
ChristianDeM <Christian.d.martino@gmail.com>
2022-04-22 00:45:39 -0500

    Agregando launch x

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res)=>{
     res.send("hola a todos ")
 })
 
+app.get('/launchx', (req, res)=>{
+    res.send("hola ahora estas en launchx")
+})
+
 app.listen(port,()=>{
     console.log("server listo calisto¡");
 })
